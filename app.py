6c004f333858863e8c35990fb2afc53b5b467588
Brandie Lucano <blucano.dev@gmail.com>
2023-12-08 12:34:58 -0800

    created Pet model and homepage

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 
 import os
 
-from flask import Flask
+from flask import Flask, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import connect_db
+from models import connect_db, Pet
 
 app = Flask(__name__)
 
@@ -19,6 +19,13 @@ connect_db(app)
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
 #
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
+
+@app.get("/")
+def home_page():
+    """List all pets"""
+
+    pets = Pet.query.all()
+    return render_template("pet_list.html", pets=pets)
